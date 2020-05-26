81626dbf46395c325d9c209b1bb71d6b5848ffbc
Kim Burgess <kim@place.technology>
2020-05-26 10:20:48 +1000

    self-host fonts
    Removes dependancy / tracking from Google Fonts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,7 +133,7 @@ module.exports = {
     },
   },
   stylesheets: [
-    "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+    '/fonts/fonts.css'
   ],
   presets: [
     [
