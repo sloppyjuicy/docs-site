98cfac132a213b87cc57dd9f35104cb669e58d41
Kim Burgess <kim@place.technology>
2020-12-08 16:50:37 +1000

    load docs content as NPM module

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
-          path: process.env.DOCS_PATH || 'docs',
+          path: process.env.npm_package_config_docs_path || 'docs',
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/placeos/docs/edit/master/',
