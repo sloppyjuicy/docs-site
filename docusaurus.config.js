2a05bf0a1dbc30901c41ca5efc914db01c6e2828
Kim Burgess <kim@place.technology>
2021-05-07 08:45:28 +1000

    feat: alternate header nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,10 +30,10 @@ module.exports = {
         srcDark: 'img/logo_dark.svg',
       },
       items: [
-        { to: 'overview', label: 'Overview' },
-        { to: 'how-to', label: 'How-To' },
-        { to: 'tutorial', label: 'Tutorials' },
-        { to: 'reference', label: 'Reference' }
+        { to: 'overview', label: 'Docs' },
+        { to: 'how-to', label: 'Drivers' },
+        { to: 'reference', label: 'API' },
+        { href: 'https://github.com/PlaceOS/PlaceOS/discussions', label: 'Community' }
       ],
     },
     hideableSidebar: true,
